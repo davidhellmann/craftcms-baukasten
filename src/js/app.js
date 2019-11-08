515402f679982c4af732ec10c3237b473e32e994
davidhellmann <david@hellmann.io>
2019-11-08 09:05:02 +0100

    .

--- src/js/app.js
+++ src/js/app.js
@@ -14,70 +14,67 @@ import 'core-js/modules/es.array.iterator';
 
 // App main
 const main = async () => {
-    // Async load the vue module
-    const {default: Vue} = await import(/* webpackChunkName: "vue" */ 'vue');
+  // Async load the vue module
+  const { default: Vue } = await import(/* webpackChunkName: "vue" */ 'vue');
 
-    // Async load LazySizes and it's plugins
-    const LazySizes = await import(
-        /* webpackChunkName: "LazySizes" */ 'lazysizes'
-        );
-    await import(
-        /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/respimg/ls.respimg.js'
-        );
-    await import(
-        /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/parent-fit/ls.parent-fit.min.js'
-        );
-    await import(
-        /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/object-fit/ls.object-fit.js'
-        );
+  // Async load LazySizes and it's plugins
+  const LazySizes = await import(
+    /* webpackChunkName: "LazySizes" */ 'lazysizes'
+  );
+  await import(
+    /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/respimg/ls.respimg.js'
+  );
+  await import(
+    /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/parent-fit/ls.parent-fit.min.js'
+  );
+  await import(
+    /* webpackChunkName: "LazySizes" */ 'lazysizes/plugins/object-fit/ls.object-fit.js'
+  );
 
-    // fix issue when image is already in viewport and content is not loaded yet
-    document.addEventListener('DOMContentLoaded', function () {
-        LazySizes.init();
-    });
+  // fix issue when image is already in viewport and content is not loaded yet
+  document.addEventListener('DOMContentLoaded', function() {
+    LazySizes.init();
+  });
 
-    // load slider async
-    if (document.getElementsByClassName('js-slider').length) {
-        await import(/* webpackChunkName: "flickity" */ './modules/flickity.js')
-            .then(flickiy => flickiy.carousel.init())
-            .catch(e => console.error(`${e.name} : ${e.message}`));
-    }
+  // load slider async
+  if (document.getElementsByClassName('js-slider').length) {
+    await import(/* webpackChunkName: "flickity" */ './modules/flickity.js')
+      .then(flickiy => flickiy.carousel.init())
+      .catch(e => console.error(`${e.name} : ${e.message}`));
+  }
 
-    // load gallery async
-    if (document.getElementsByClassName('js-gallery').length) {
-        await import(
-            /* webpackChunkName: "photoswipe" */ './modules/photoswipe.js'
-            )
-            .then(photoswipe => photoswipe.default.init('.js-gallery'))
-            .catch(e => console.error(`${e.name} : ${e.message}`));
-    }
+  // load gallery async
+  if (document.getElementsByClassName('js-gallery').length) {
+    await import(/* webpackChunkName: "photoswipe" */ './modules/photoswipe.js')
+      .then(photoswipe => photoswipe.default.init('.js-gallery'))
+      .catch(e => console.error(`${e.name} : ${e.message}`));
+  }
 
-    // Create our vue instance
-    const app = new Vue({
-        el: "#app",
-        delimiters: ['${', '}'],
-        components: {
-            'confetti': () =>
-                import(
-                    /* webpackChunkName: "confetti" */
-                    '../vue/Confetti.vue'
-                ),
-        },
-        data: {},
-        methods: {},
-        mounted() {
-        },
-    });
+  // Create our vue instance
+  const app = new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    components: {
+      confetti: () =>
+        import(
+          /* webpackChunkName: "confetti" */
+          '../vue/Confetti.vue'
+        ),
+    },
+    data: {},
+    methods: {},
+    mounted() {},
+  });
 
-    return app;
+  return app;
 };
 
 // Execute async function
-main().then((app) => {
-    console.log('async main executed');
+main().then(app => {
+  console.log('async main executed');
 });
 
 // Accept HMR as per: https://webpack.js.org/api/hot-module-replacement#accept
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
