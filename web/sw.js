6a9b1d46b4d3c5aaa08927be28749d3e291f70d0
David Hellmann <davidhellmann.com@gmail.com>
2020-01-23 23:17:28 +0100

    Remove Unused Stuff

--- web/sw.js
+++ web/sw.js
@@ -11,12 +11,18 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
   "/dist/js/workbox-catch-handler.js",
-  "/dist/js/precache-manifest.7fab7918899ff5ac519e4c58ebed6da6.js"
+  "/dist/js/precache-manifest.d1787b8e63eed3c2558f17d3053a7293.js"
 );
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -24,11 +30,10 @@ importScripts(
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(/\/admin.*$/, workbox.strategies.networkOnly(), 'GET');
-workbox.routing.registerRoute(/\.php$/, workbox.strategies.networkOnly(), 'GET');
-workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg|webp)$/, workbox.strategies.cacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({"maxEntries":20,"purgeOnQuotaError":false})] }), 'GET');
+workbox.routing.registerRoute(/\/admin.*$/, new workbox.strategies.NetworkOnly(), 'GET');
+workbox.routing.registerRoute(/\.php$/, new workbox.strategies.NetworkOnly(), 'GET');
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg|webp)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [new workbox.expiration.Plugin({ maxEntries: 20, purgeOnQuotaError: false })] }), 'GET');
 
 workbox.googleAnalytics.initialize({});
