c8bb6c05576a362cc00d49cb37ab1a9971a9b56a
davidhellmann <david@hellmann.io>
2020-04-03 00:00:45 +0200

    Update

--- src/inlineJs/load-fonts.js
+++ src/inlineJs/load-fonts.js
@@ -1,9 +1,14 @@
-"use strict";
+'use strict';
 
-var FontA = new FontFaceObserver("Merriweather");
-var FontB = new FontFaceObserver("Lato");
-var FontC = new FontFaceObserver("Roboto Mono");
+var FontA = new FontFaceObserver('Merriweather');
+var FontB = new FontFaceObserver('Lato');
+var FontC = new FontFaceObserver('Roboto Mono');
 
-Promise.all([FontA.load(), FontB.load(), FontC.load()]).then(function () {
-    document.documentElement.className += "fonts-loaded";
-});
+Promise.all([FontA.load(), FontB.load(), FontC.load()])
+  .then(function() {
+    document.documentElement.classList.add('fonts-loaded');
+  })
+  .catch(e => {
+    console.error(e);
+    document.documentElement.classList.add('fonts-failed');
+  });
