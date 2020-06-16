5716d84f1f2dcda7122fd7ec3522a819ff100693
davidhellmann <david@hellmann.io>
2020-06-16 15:24:16 +0200

    Back to oldsql

--- config/db.php
+++ config/db.php
@@ -9,9 +9,12 @@
  */
 
 return [
-    'dsn' => getenv('DB_DSN'),
+    'driver' => getenv('DB_DRIVER'),
+    'server' => getenv('DB_SERVER'),
     'user' => getenv('DB_USER'),
     'password' => getenv('DB_PASSWORD'),
+    'database' => getenv('DB_DATABASE'),
     'schema' => getenv('DB_SCHEMA'),
     'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+    'port' => getenv('DB_PORT')
 ];
