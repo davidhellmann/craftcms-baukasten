a67c2cc110222b817447d9dea7cc434ff21e7b52
davidhellmann <david@hellmann.io>
2020-08-28 09:42:46 +0200

    Baukasten Styles

--- config/redactor/plugins/baukasten/baukasten.js
+++ config/redactor/plugins/baukasten/baukasten.js
@@ -5,48 +5,136 @@
       this.app = app;
       this.toolbar = app.toolbar;
       this.selection = app.selection;
+
+      this.styles = {
+        marked: {
+          title: 'Marked',
+          api: 'module.inline.format',
+          args: {
+            tag: 'mark',
+            class: 'rd-mark',
+          },
+        },
+        variable: {
+          title: 'Variable',
+          api: 'module.inline.format',
+          args: {
+            tag: 'var',
+            class: 'rd-var',
+          },
+        },
+        shortcut: {
+          title: 'Shortcut',
+          api: 'module.inline.format',
+          args: {
+            tag: 'kbd',
+            class: 'rd-kbd',
+          },
+        },
+        superscript: {
+          title: 'Superscript',
+          api: 'module.inline.format',
+          args: {
+            tag: 'sup',
+            class: 'rd-sup',
+          },
+        },
+        subscript: {
+          title: 'Subscript',
+          api: 'module.inline.format',
+          args: {
+            tag: 'sub',
+            class: 'rd-sub',
+          },
+        },
+        deleted: {
+          title: 'Deleted',
+          api: 'module.inline.format',
+          args: {
+            tag: 'del',
+            class: 'rd-del',
+          },
+        },
+        smallPrint: {
+          title: 'Small',
+          api: 'module.inline.format',
+          args: {
+            tag: 'small',
+            class: 'rd-small',
+          },
+        },
+        favorite: {
+          title: 'Favorite',
+          api: 'module.inline.format',
+          args: {
+            tag: 'span',
+            class: 'rd-favorite favorite',
+            type: 'toggle',
+          },
+        },
+        clearStyles: {
+          title: 'Clear Styles',
+          api: 'module.inline.clearformat',
+        },
+      };
     },
 
     start: function() {
       var dropdownData = {
         primary: {
-          title: 'Primary',
+          title: 'Link Primary',
           api: 'plugin.baukasten.togglePrimary',
         },
         secondary: {
-          title: 'Secondary',
+          title: 'Link Secondary',
           api: 'plugin.baukasten.toggleSecondary',
         },
         outlined: {
-          title: 'Outlined',
+          title: 'Link Outlined',
           api: 'plugin.baukasten.toggleOutlined',
         },
       };
 
+      for (var key in this.styles) {
+        var style = this.styles[key];
+        dropdownData[key] = {
+          title: style.title,
+          args: style.args,
+          api: style.api,
+        };
+      }
+
       var btnData = {
-        title: 'Button',
+        title: 'Custom Styles',
       };
 
       // create the button
-      var $button = this.toolbar.addButton('baukasten', btnData);
+      var $button = this.toolbar.addButtonAfter('format', 'baukasten', btnData);
 
       // set dropdown
       $button.setDropdown(dropdownData);
     },
 
     togglePrimary: function() {
-      this.toggle('c-link--primary', 'a');
+      this.toggleLinkStyles('rd-link  rd-link--primary', 'a');
     },
 
     toggleSecondary: function() {
-      this.toggle('c-link--secondary', 'a');
+      this.toggleLinkStyles('rd-link  rd-link--secondary', 'a');
     },
 
     toggleOutlined: function() {
-      this.toggle('c-link--outlined', 'a');
+      this.toggleLinkStyles('rd-link  rd-link--outlined', 'a');
     },
 
-    toggle: function(classToToggle, element) {
+    // Other Styles
+    toggleStyles: function(classToToggle, element) {
+      var obj = { tag: element, class: classToToggle };
+      this.inline.format(obj);
+    },
+
+    // Link Styles
+    toggleLinkStyles: function(classToToggle, element) {
       var selectedLinks = this.selection.getInlines({
         all: true,
         tags: [element],
