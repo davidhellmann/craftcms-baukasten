e1b1eb3f69047ef616043399841988429ec6095d
davidhellmann <david@hellmann.io>
2020-09-03 00:38:50 +0200

    Linter :D

--- config/redactor/plugins/baukasten/baukasten.js
+++ config/redactor/plugins/baukasten/baukasten.js
@@ -5,7 +5,7 @@
       this.app = app;
       this.toolbar = app.toolbar;
       this.selection = app.selection;
-      this.headings = ['h1','h2','h3','h4','h5','h6']
+      this.headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
       this.styles = {
         marked: {
