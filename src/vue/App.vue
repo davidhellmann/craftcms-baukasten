deeb898a9acf6373d75d6794f304184f95398e15
davidhellmann <david@hellmann.io>
2020-12-13 10:39:42 +0100

    Init

---
+++ src/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <confetti />
+</template>
+
+<script lang="ts">
+// Async load the Vue 3 APIs we need from the Vue ESM
+import { defineAsyncComponent, defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    confetti: defineAsyncComponent(
+      () => import(/* webpackChunkName: "confetti" */ './Confetti.vue'),
+    ),
+  },
+  data: () => ({}),
+  mounted() {},
+  methods: {},
+});
+</script>
