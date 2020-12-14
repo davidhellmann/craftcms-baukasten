493c27ad90dbdbf06a3fe60aaaba009ebda0ec96
davidhellmann <david@hellmann.io>
2020-12-14 22:59:31 +0100

    TW Script

--- src/js/_vendors.ts
+++ src/js/_vendors.ts
@@ -1,27 +1,7 @@
-// Async load LazySizes and it's plugins
-if (document.querySelectorAll('.lazyload').length) {
-  const nativeLazy = false;
-  if ('loading' in HTMLImageElement.prototype && nativeLazy) {
-    const images = [...document.querySelectorAll('img[loading="lazy"]')];
-    images.forEach(img => {
-      img.src = img.dataset.src || img.src;
-      img.srcset = img.dataset.srcset;
-      img.sizes = `${img.getBoundingClientRect().width}px`;
-    });
-  } else {
-    const LazySizes = import(/* webpackChunkName: "LazySizes" */ 'lazysizes');
+const vendors = {
+  init() {
+    //
+  },
+};
 
-    /*import(
-      /!* webpackChunkName: "LazySizes" *!/ 'lazysizes/plugins/respimg/ls.respimg.js'
-    );*/
-
-    /*import(
-      /!* webpackChunkName: "LazySizes" *!/ 'lazysizes/plugins/object-fit/ls.object-fit.js'
-    );*/
-
-    // fix issue when image is already in viewport and content is not loaded yet
-    document.addEventListener('DOMContentLoaded', () => {
-      LazySizes.init();
-    });
-  }
-}
+export default vendors;
