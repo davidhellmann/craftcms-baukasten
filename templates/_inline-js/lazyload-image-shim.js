65cc338de46932b564c8d430ff6c8fe9a1634656
davidhellmann <david@hellmann.io>
2020-12-17 01:46:19 +0100

    Add First Components

--- templates/_inline-js/lazyload-image-shim.js
+++ templates/_inline-js/lazyload-image-shim.js
@@ -1,19 +1,21 @@
 // From https://web.dev/native-lazy-loading/#how-do-i-handle-browsers-that-don't-yet-support-native-lazy-loading
 if ('loading' in HTMLImageElement.prototype) {
-    // Replace the img.src with what is in the data-src property
-    const images = document.querySelectorAll('img[loading="lazy"]');
-    images.forEach(img => {
-        img.src = img.dataset.src;
-    });
-    // Replace the source.srcset with what is in the data-srcset property
-    const sources = document.querySelectorAll('source[data-srcset]')
-    sources.forEach(source => {
-        source.srcset = source.dataset.srcset;
-    });
+  // Replace the img.src with what is in the data-src property
+  const images = document.querySelectorAll('img[loading="lazy"]');
+  images.forEach(img => {
+    img.srcset = img.dataset.srcset;
+  });
+  // Replace the source.srcset with what is in the data-srcset property
+  const sources = document.querySelectorAll('source[data-srcset]');
+  sources.forEach(source => {
+    source.srcset = source.dataset.srcset;
+  });
 } else {
-   // Dynamically import the LazySizes library
-    const script = document.createElement('script');
-    script.src =
-        '{{ craft.twigpack.getModuleUri("lazysizes-wrapper.js", "legacy") }}';
-    document.body.appendChild(script);
+  // Dynamically import the LazySizes library
+  const script = document.createElement('script');
+  script.src =
+    '{{ craft.twigpack.getModuleUri("lazysizes-wrapper.js", "legacy") }}';
+  document.body.appendChild(script);
 }
+
+console.log('loading' in HTMLImageElement.prototype);
