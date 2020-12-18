df6825bf655448379a589c7130a0078a550df9c9
davidhellmann <david@hellmann.io>
2020-12-18 16:02:15 +0100

    Update

---
+++ templates/_components/code/translate.en.component.code.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'translate.component.code.show' => 'Show',
+    'translate.component.code.hide' => 'Hide',
+    'translate.component.code.copy' => 'Copy',
+    'translate.component.code.copied' => '✅ Copied!',
+];
+?>
---
+++ templates/_components/link/translate.en.component.link.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    "translate.component.linkIcon.text" => "Link Icon",
+];
+?>
