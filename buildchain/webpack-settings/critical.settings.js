1899a81c50af7fdd558c07a6c813d706b719cd37
davidhellmann <david@hellmann.io>
2020-12-18 16:35:42 +0100

    Move all stuff into Buildchain Folder

--- buildchain/webpack-settings/critical.settings.js
+++ buildchain/webpack-settings/critical.settings.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // settings
 module.exports = {
   critical: {
-    base: './web/dist/criticalcss/',
+    base: '../web/dist/criticalcss/',
     suffix: '_critical.min.css',
     criticalHeight: 1200,
     criticalWidth: 1200,
--- buildchain/webpack-settings/dev-server.settings.js
+++ buildchain/webpack-settings/dev-server.settings.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // settings
 module.exports = {
-  contentBase: () => process.env.DEVSERVER_CONTENT_BASE || '../templates/',
+  contentBase: () => process.env.DEVSERVER_CONTENT_BASE || '../../templates/',
   host: () => process.env.DEVSERVER_HOST || 'localhost',
   https: () => process.env.DEVSERVER_HTTPS || false,
   poll: () => process.env.DEVSERVER_POLL || false,
