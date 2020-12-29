7336d5cf5e0985c14bea5a7e652fa9750fb5e7d3
davidhellmann <david@hellmann.io>
2020-12-29 01:58:57 +0100

    Fix Paths

--- buildchain/webpack-configs/build.config.js
+++ buildchain/webpack-configs/build.config.js
@@ -6,43 +6,43 @@ const path = require('path');
 
 // return a webpack config
 module.exports = (type = 'modern', settings) => {
-    // common config
-    const common = () => ({
-        entry: {},
-        mode: 'production',
-        output: {
-            path: path.resolve(__dirname, settings.paths.dist),
-        },
-        resolve: {
-            modules: [
-                path.resolve(__dirname, '../node_modules'),
-            ],
-        },
-    });
-    // configs
-    const configs = {
-        // development configs
-        development: {
-            // legacy development config
-            legacy: {
-            },
-            // modern development config
-            modern: {
-                ...common(),
-            },
-        },
-        // production configs
-        production: {
-            // legacy production config
-            legacy: {
-                ...common(),
-            },
-            // modern production config
-            modern: {
-                ...common(),
-            },
-        }
-    };
+  // common config
+  const common = () => ({
+    entry: {},
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, settings.paths.dist),
+      publicPath: settings.urls.publicPath()
+    },
+    resolve: {
+      modules: [
+        path.resolve(__dirname, '../node_modules'),
+      ],
+    },
+  });
+  // configs
+  const configs = {
+    // development configs
+    development: {
+      // legacy development config
+      legacy: {},
+      // modern development config
+      modern: {
+        ...common(),
+      },
+    },
+    // production configs
+    production: {
+      // legacy production config
+      legacy: {
+        ...common(),
+      },
+      // modern production config
+      modern: {
+        ...common(),
+      },
+    }
+  };
 
-    return configs[process.env.NODE_ENV][type];
+  return configs[process.env.NODE_ENV][type];
 }
