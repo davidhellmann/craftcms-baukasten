5965edaa9e29bdae21458ea78650bcdf92bfcaeb
davidhellmann <david@hellmann.io>
2021-01-03 01:28:55 +0100

    Add Dummy Vue Component

---
+++ src/vue/DummySubComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="text-2xl font-bold">Vue Dummy(Sub)Component</h1>
+  <input v-model="message" placeholder="Type here..." class="border-2 border-black px-2 py-1" />
+  <p>Message is: {{ message }}</p>
+</template>
+
+<script lang="ts">
+// Async load the Vue 3 APIs we need from the Vue ESM
+import { ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  setup(props) {
+    const message = ref('Dummy Text')
+    return {
+      message
+    }
+  },
+  mounted() {},
+  methods: {},
+});
+</script>
