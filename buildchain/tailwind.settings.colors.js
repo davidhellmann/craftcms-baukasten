3006da2e1b759f7ea68b7c361e5538a51af85efb
davidhellmann <david@hellmann.io>
2021-09-01 00:47:56 +0200

    Update Image Comp

--- buildchain/tailwind.settings.colors.js
+++ buildchain/tailwind.settings.colors.js
@@ -2,7 +2,13 @@
  * Tailwind Color Settings
  */
 
-const { black, white, warmGray } = require('tailwindcss/colors');
+const {
+  black,
+  white,
+  warmGray,
+  emerald,
+  yellow,
+} = require('tailwindcss/colors');
 
 module.exports = {
   current: 'currentColor',
@@ -10,22 +16,6 @@ module.exports = {
   black,
   white,
   gray: warmGray,
-  beige: {
-    100: '#F6F4F3',
-    500: '#E9E2E2',
-  },
-  yellow: {
-    500: '#FFEC2D',
-    600: '#ebd82d',
-  },
-  green: {
-    500: '#19BD0A',
-    600: '#19a90a',
-  },
-  blue: {
-    200: '#B8CFDA',
-    300: '#a4bbc6',
-    800: '#003B5C',
-    900: '#002748',
-  },
+  primary: emerald,
+  secondary: yellow,
 };
---
+++ config/patternlib.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Patternlib
+ */
+
+return [
+    'pageTitle' => 'Pattern Library',
+    'navHeadline' => 'Components',
+    'path' => '/dev/patternlib/',
+    'navigation' => [
+        'components' => [
+            'title' => 'Components',
+            'childs' => [
+                'buttons' => [
+                    'url' => 'components/buttons',
+                    'title' => 'Buttons'
+                ],
+                'images' => [
+                    'url' => 'components/images',
+                    'title' => 'Images'
+                ],
+                'galleries' => [
+                    'url' => 'components/galleries',
+                    'title' => 'Galleries'
+                ],
+                'spacers' => [
+                    'url' => 'components/spacers',
+                    'title' => 'Spacers'
+                ],
+                'stacks' => [
+                    'url' => 'components/stacks',
+                    'title' => 'Stacks'
+                ],
+            ]
+        ],
+        'views' => [
+            'title' => 'Views',
+            'childs' => [
+                'buttons' => [
+                    'url' => 'components/buttons',
+                    'title' => 'Buttons'
+                ],
+                'images' => [
+                    'url' => 'components/images',
+                    'title' => 'Images'
+                ],
+            ]
+        ],
+    ],
+];
