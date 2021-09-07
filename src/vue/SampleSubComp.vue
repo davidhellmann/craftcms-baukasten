78b303b1ac24e0eaed098f33178d956ebca3f82c
davidhellmann <david@hellmann.io>
2021-09-07 14:13:05 +0200

    Add Vue Sample to home

--- src/vue/SampleSubComp.vue
+++ src/vue/SampleSubComp.vue
@@ -1,21 +1,10 @@
 <template>
   <h1 class="text-2xl font-bold">Vue Sample(Sub)Component</h1>
-  <input v-model="message" placeholder="Type here..." class="border-2 border-black px-2 py-1" />
+  <input v-model="message" placeholder="Type here..." class="border-2 border-black px-2 py-1"/>
   <p>Message is: {{ message }}</p>
 </template>
 
-<script lang="ts">
-// Async load the Vue 3 APIs we need from the Vue ESM
-import { ref, defineComponent } from 'vue';
-
-export default defineComponent({
-  setup(props) {
-    const message = ref('')
-    return {
-      message
-    }
-  },
-  mounted() {},
-  methods: {},
-});
+<script lang="ts" setup>
+import {ref} from 'vue';
+const message = ref('');
 </script>
