9713847974fb27e69a61cf4567040be5aff3f55d
davidhellmann <david@hellmann.io>
2021-09-07 06:03:19 +0200

    Remove Code Comp (to special :D)

--- templates/_components/code/translate.en.component.code.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'translate.component.code.show' => 'Show',
-    'translate.component.code.hide' => 'Hide',
-    'translate.component.code.copy' => 'Copy',
-    'translate.component.code.copied' => '✅ Copied!',
-];
