b190e494482a47b6df85436f981d0c70729daaf8
davidhellmann <david@hellmann.io>
2021-09-07 14:13:05 +0200

    Add Vue Sample to home

--- src/vue/SampleComp.vue
+++ src/vue/SampleComp.vue
@@ -1,18 +1,7 @@
 <template>
-  <SampleSubComp />
+  <SampleSubComp/>
 </template>
 
-<script lang="ts">
-// Async load the Vue 3 APIs we need from the Vue ESM
-import { defineAsyncComponent, defineComponent } from 'vue';
-
-export default defineComponent({
-  components: {
-    SampleSubComp: defineAsyncComponent(
-      () => import(/* webpackChunkName: "sampleSubComp" */ '@/vue/SampleSubComp.vue'),
-    ),
-  },
-  mounted() {},
-  methods: {},
-});
+<script lang="ts" setup>
+import SampleSubComp from '@/vue/SampleSubComp.vue';
 </script>
