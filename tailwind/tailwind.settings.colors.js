f6b27d98d63efd2932934c7616981b8af153c4dc
davidhellmann <david@hellmann.io>
2021-11-03 15:09:26 +0100

    Disable ESLINT

--- tailwind/tailwind.settings.colors.js
+++ tailwind/tailwind.settings.colors.js
@@ -2,12 +2,10 @@
  * Tailwind Color Settings
  */
 
-const generateColorClass = (variable) => {
-  return ({ opacityValue }) =>
-    opacityValue
-      ? `rgba(var(--${variable}), ${opacityValue})`
-      : `rgb(var(--${variable}))`
-}
+const generateColorClass = (variable) =>  ({opacityValue}) => opacityValue
+  ? `rgba(var(--${variable}), ${opacityValue})`
+  : `rgb(var(--${variable}))`
+
 
 module.exports = {
   current: 'currentColor',
@@ -21,7 +19,7 @@ module.exports = {
     300: generateColorClass('c-gray-300'),
     400: generateColorClass('c-gray-400'),
     500: generateColorClass('c-gray-500'),
-    'DEFAULT': generateColorClass('c-gray-500'),
+    DEFAULT: generateColorClass('c-gray-500'),
     600: generateColorClass('c-gray-600'),
     700: generateColorClass('c-gray-700'),
     800: generateColorClass('c-gray-800'),
@@ -34,7 +32,7 @@ module.exports = {
     300: generateColorClass('c-primary-300'),
     400: generateColorClass('c-primary-400'),
     500: generateColorClass('c-primary-500'),
-    'DEFAULT': generateColorClass('c-primary-500'),
+    DEFAULT: generateColorClass('c-primary-500'),
     600: generateColorClass('c-primary-600'),
     700: generateColorClass('c-primary-700'),
     800: generateColorClass('c-primary-800'),
@@ -47,7 +45,7 @@ module.exports = {
     300: generateColorClass('c-secodary-300'),
     400: generateColorClass('c-secodary-400'),
     500: generateColorClass('c-secodary-500'),
-    'DEFAULT': generateColorClass('c-secodary-500'),
+    DEFAULT: generateColorClass('c-secodary-500'),
     600: generateColorClass('c-secodary-600'),
     700: generateColorClass('c-secodary-700'),
     800: generateColorClass('c-secodary-800'),
