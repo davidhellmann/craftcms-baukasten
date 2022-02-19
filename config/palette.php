548767552181c560716ed1ec47aec62ab8124241
davidhellmann <david@hellmann.io>
2022-02-19 17:51:05 +0100

    ⬆️ upgrade: craftcms

---
+++ config/palette.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Palette config.php
+ *
+ * This file exists only as a template for the Palette settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'palette.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    // If Palette should be enabled on the frontend of the website
+    'enableOnFrontend' => true,
+
+    // If Palette should be enabled on the backend of the website
+    'enableOnBackend' => true,
+];
