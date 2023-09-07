f17ac0d8d1be3e43d34f6ae4019da49c344c2932
David Hellmann <david@hellmann.io>
2023-09-07 21:34:19 +0200

    🔧 config: make redis (totally) optional

--- config/app.php
+++ config/app.php
@@ -19,6 +19,43 @@
 
 use craft\helpers\App;
 
+$redisConfig = [
+    'redis' => [
+        'class' => yii\redis\Connection::class,
+        'hostname' => App::env('REDIS_HOSTNAME'),
+        'port' => App::env('REDIS_PORT'),
+        'database' => App::env('REDIS_DEFAULT_DB'),
+    ],
+    'cache' => [
+        'class' => yii\redis\Cache::class,
+        'keyPrefix' => '_' . App::env('CRAFT_APP_ID') . '_CACHE_' ?: 'CraftCMS_CACHE_',
+        'defaultDuration' => 86400,
+        'redis' => 'redis',
+    ],
+    'mutex' => static function() {
+        $config = [
+            'class' => craft\mutex\Mutex::class,
+            'mutex' => [
+                'class' => yii\redis\Mutex::class,
+                // set the max duration to 15 minutes for console requests
+                'expire' => Craft::$app->request->isConsoleRequest ? 900 : 30,
+                'redis' => [
+                    'hostname' => App::env('REDIS_HOSTNAME') ?: 'localhost',
+                    'port' => App::env('REDIS_PORT'),
+                    'password' => App::env('REDIS_PASSWORD') ?: null,
+                    'database' => App::env('REDIS_DEFAULT_DB'),
+                ],
+            ],
+        ];
+
+        // Return the initialized component:
+        return Craft::createObject($config);
+    },
+];
+
+$redisIsUsed = App::env('REDIS_HOSTNAME') && App::env('REDIS_PORT');
+
+
 return [
     '*' => [
         'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
@@ -42,38 +79,7 @@ return [
                 'class' => craft\queue\Queue::class,
                 'ttr' => 10 * 60,
             ],
-            'redis' => [
-                'class' => yii\redis\Connection::class,
-                'hostname' => App::env('REDIS_HOSTNAME'),
-                'port' => App::env('REDIS_PORT'),
-                'database' => App::env('REDIS_DEFAULT_DB'),
-            ],
-            'cache' => [
-                'class' => yii\redis\Cache::class,
-                'keyPrefix' => '_' . App::env('CRAFT_APP_ID') . '_CACHE_' ?: 'CraftCMS_CACHE_',
-                'defaultDuration' => 86400,
-                'redis' => 'redis',
-            ],
-            'mutex' => static function() {
-                $config = [
-                    'class' => craft\mutex\Mutex::class,
-                    'mutex' => [
-                        'class' => yii\redis\Mutex::class,
-                        // set the max duration to 15 minutes for console requests
-                        'expire' => Craft::$app->request->isConsoleRequest ? 900 : 30,
-                        'redis' => [
-                            'hostname' => App::env('REDIS_HOSTNAME') ?: 'localhost',
-                            'port' => App::env('REDIS_PORT'),
-                            'password' => App::env('REDIS_PASSWORD') ?: null,
-                            'database' => App::env('REDIS_DEFAULT_DB'),
-                        ],
-                    ],
-                ];
-
-                // Return the initialized component:
-                return Craft::createObject($config);
-            },
-        ],
+        ] + ($redisIsUsed ? $redisConfig : []),
     ],
     'production' => [],
     'staging' => [],
