0449cb32edf0fdbcb6026234aeb7dc582e373709
David Hellmann <david@hellmann.io>
2025-11-13 22:25:15 +0100

    wip

--- src/js/_components.ts
+++ src/js/_components.ts
@@ -1,13 +1,13 @@
 export default (async () => {
   // Gallery
-  const imageGalleryEls = document.querySelectorAll<HTMLElement>('.js-imageGallery');
+  const imageGalleryEls = document.querySelectorAll<HTMLElement>('[data-js="image-gallery"]');
   if (imageGalleryEls.length > 0) {
     const { default: imageGallery } = await import('../../templates/_components/gallery/images');
     imageGallery.init(imageGalleryEls);
   }
 
   // swiperHolder
-  const swiperHolderEls = document.querySelectorAll<HTMLElement>('.js-swiperHolder');
+  const swiperHolderEls = document.querySelectorAll<HTMLElement>('[data-js="swiper-holder"]');
   if (swiperHolderEls.length > 0) {
     const { default: swiperHolder } = await import(
       '../../templates/_components/slider/swiperHolder'
@@ -15,17 +15,8 @@ export default (async () => {
     swiperHolder.init(swiperHolderEls);
   }
 
-  // Navigation Main
-  const navigationMainEl = document.querySelector<HTMLElement>('.js-navigationMain');
-  if (navigationMainEl) {
-    const { default: navigationMain } = await import(
-      '../../templates/_components/navigation/navigationMain'
-    );
-    navigationMain.init(navigationMainEl);
-  }
-
   // Code
-  const codeBlockEls = document.querySelectorAll<HTMLElement>('.js-code');
+  const codeBlockEls = document.querySelectorAll<HTMLElement>('[data-js="code"]');
   if (codeBlockEls.length > 0) {
     const { default: code } = await import('../../templates/_components/text/code');
     code.init(codeBlockEls);
